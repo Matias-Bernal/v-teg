8c7b56404abe417e564a00815a69cb0f08715bdb
lucianodb3@gmail.com <lucianodb3@gmail.com@7f5bd730-5ec6-4020-333b-02943f3541ab>
2013-06-24 01:44:55 +0000

    primer prototipo de Estado del juego para la transferencia.

--- trunk/MODEL/State.java
+++ trunk/MODEL/State.java
@@ -1,5 +1,53 @@
 package MODEL;
 
 public class State {
+	IPlayer currentPlayer;
+	int cantPlayer;
+	/*0 -estado inicial
+	 *1 -estado que mantiene el cliente
+	 *2 -estado que mantiene el server
+	 *3 -broadcast de cambio de estados se mantiene el mismo jugador
+	 *4 -broadcast de cambio de estados pasa al nuevo jugador */
+	int stateType;
+	boolean conquest=false;
+	Game game;
+	
+	
+	
+	public State() {
+	}
+	
+	public IPlayer getCurrentPlayer() {
+		return currentPlayer;
+	}
+	public void setCurrentPlayer(IPlayer currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+	public int getCantPlayer() {
+		return cantPlayer;
+	}
+	public void setCantPlayer(int cantPlayer) {
+		this.cantPlayer = cantPlayer;
+	}
+	public int getStateType() {
+		return stateType;
+	}
+	public void setStateType(int stateType) {
+		this.stateType = stateType;
+	}
+	public Game getGame() {
+		return game;
+	}
+	public void setGame(Game game) {
+		this.game = game;
+	}
 
+	public boolean isConquest() {
+		return conquest;
+	}
+
+	public void setConquest(boolean conquest) {
+		this.conquest = conquest;
+	}
+	
 }
