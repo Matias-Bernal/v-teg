84ba080ab9b0c9ee27ea9a876adb08f1c3cf1c26
lucianodb3@gmail.com <lucianodb3@gmail.com@7f5bd730-5ec6-4020-333b-02943f3541ab>
2013-06-24 04:14:10 +0000

    ACTUALIZACION DEL MODELO DE ESTADO, incorporacion de atributos extras en la tarjeta y las tarjetas correspondiente a el player y se agregaron las clases para el modelo de estado compacto y estado completo

--- MODEL/Card.java
+++ MODEL/Card.java
@@ -6,39 +6,41 @@ public class Card implements Serializable  {
 
 	private static final long serialVersionUID = 1L;
 	Country country;
+	boolean changed=false;//cambiar este valor cuando se retorne las tarjetas mazo
 	int type;
 	
 	public Card(Country c,int t) {
 		country=c;
 		type=t;
 	}
-
 	
 	public long get_id (){
 		return serialVersionUID;
 	}
 	
-	
-	
 	public Country get_Country() {
 		return country;
 	}
-
 
 	public void set_Country(Country country) {
 		this.country = country;
 	}
 
-
 	public int get_Type() {
 		return type;
 	}
 
-
 	public void set_Type(int type) {
 		this.type = type;
 	}
+	
+	public boolean is_Changed() {
+		return changed;
+	}
 
+	public void set_Changed(boolean changed) {
+		this.changed = changed;
+	}
 
 	public boolean repOK(){
 		return type>0 && type<5;
